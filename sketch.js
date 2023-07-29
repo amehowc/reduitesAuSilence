122b5dd55fa3e93366f6aa9f4ee1f0cd80da8a5d
Arthur Cloche <arthurcloche@gmail.com>
2023-07-29 12:39:16 -0400

    fix: change UI for targeting

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
 
 document.addEventListener("DOMContentLoaded", function() {
   const scene = document.querySelector('#scene');
-  const target = document.querySelector('#my-marker');
-  const video = document.querySelector('#target-practice');
+  const target = document.querySelector('#target');
+  const video = document.querySelector('#video-content');
   video.pause();
   video.currentTime = 0;
 
   scene.addEventListener("arReady", event => {
-
-    const loadingHolder = document.querySelector('#overlay');
+    const loadingHolder = document.querySelector('#loading-overlay');
     loadingHolder.classList.add("hidden")
 	  console.log("MindAR is ready")
 	});
+  
   // Detect target found
   target.addEventListener("targetFound", event => {
     console.log("target found");
