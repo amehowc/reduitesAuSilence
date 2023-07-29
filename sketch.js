2070c5fef159ef5fd6434881dbfd46051e0dff04
Arthur Cloche <arthurcloche@gmail.com>
2023-07-29 12:43:36 -0400

    fix: troubleshooting size

--- sketch.js
+++ sketch.js
@@ -11,17 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
     loadingHolder.classList.add("hidden")
 	  console.log("MindAR is ready")
 	});
-  
-  // Detect target found
+
   target.addEventListener("targetFound", event => {
-    console.log("target found");
     video.play();
   });
 
-  // Detect target lost
   target.addEventListener("targetLost", event => {
-    console.log("target lost");
     video.pause();
-    video.currentTime = 0; // Reset video playback to the beginning
+    video.currentTime = 0;
   });
 });
