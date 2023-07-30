f741c464a585390249b86ee9c31504362aefb0ea
Arthur Cloche <arthurcloche@gmail.com>
2023-07-30 12:08:37 -0400

    fix: add eventlistener and gesture checking overlay

--- sketch.js
+++ sketch.js
@@ -1,22 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-document.addEventListener("DOMContentLoaded", function() {
-  const scene = document.querySelector('#scene');
-  const target = document.querySelector('#target');
-  const video = document.querySelector('#video-content');
+  const scene = document.querySelector("#scene");
+  const target = document.querySelector("#target");
+  const video = document.querySelector("#video-content");
+  const playButton = document.querySelector("#play-button");
+  const checkingOverlay = document.querySelector("#checking-overlay");
   video.pause();
   video.currentTime = 0;
+  checkingOverlay.addEventListener("click", () => {
+    if (typeof video.play === "function") {
+      video.play();
+    }
+    checkingOverlay.classList.add("hidden")
+  });
 
-  scene.addEventListener("arReady", event => {
-    const loadingHolder = document.querySelector('#loading-overlay');
-    loadingHolder.classList.add("hidden")
-	  console.log("MindAR is ready")
-	});
+  scene.addEventListener("arReady", (event) => {
+    console.log("MindAR is ready");
+    checkingOverlay.classList.remove("hidden")
+  });
 
-  target.addEventListener("targetFound", event => {
+  target.addEventListener("targetFound", (event) => {
     video.play();
   });
 
-  target.addEventListener("targetLost", event => {
+  target.addEventListener("targetLost", (event) => {
     video.pause();
     video.currentTime = 0;
   });
